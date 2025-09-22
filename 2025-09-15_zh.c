4cd5d7c1a8384cd6fe01fe7fdd9c7585e460bdc3
Mayer Hedda <mayer.hedda@gmail.com>
2025-09-22 13:37:24 +0200

    órai munka

---
+++ 2025-09-15_zh.c
@@ -0,0 +1,9 @@
+// Zsákos Bilbó szülinapot szervez
+// Írj C programot ami felköszönti
+
+#include <stdio.h>
+
+int main() {
+    printf("Boldog születésnapot, Bilbó!\n");
+    return 0;
+}
---
+++ 2025-09-22_gyak.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+    scanf("%d", &c);
+    printf("%d karakterként: %c\n", c, c);
+    return 0;
+}
