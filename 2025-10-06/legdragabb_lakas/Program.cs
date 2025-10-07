6c6dc4a02e037cf5ec3fed01a13a9ac74d0a9efd
Mayer Hedda <mayer.hedda@gmail.com>
2025-10-07 18:14:13 +0200

    2025-10-06 órai feladat

---
+++ 2025-10-06/legdragabb_lakas/Program.cs
@@ -0,0 +1,19 @@
+﻿int n;
+int.TryParse(Console.ReadLine(), out n);
+int[] arak = new int[n];
+
+for (int i = 0; i < arak.Length; i++) int.TryParse(Console.ReadLine(), out arak[i]);
+
+int maxi = 0;
+int maxe = arak[0];
+
+for (int i = 1; i < arak.Length; i++)
+{
+    if (arak[i] > maxe)
+    {
+        maxi = i;
+        maxe = arak[i];
+    }
+}
+
+Console.WriteLine(maxi);
