73df669fe4855f6b31a72e4626d08532dfd19bb4
Mayer Hedda <mayer.hedda@gmail.com>
2025-10-22 11:06:30 +0200

    progalap házi

---
+++ ProgAlap/Gyak/ANapAmikorNemVoltKapas/ANapAmikorNemVoltKapas/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ANapAmikorNemVoltKapas;
+/*
+ * Specifikáció: https://progalap.elte.hu/specifikacio/v1/?uuid=bb49ba74-3440-4fc7-81a7-9012bf8d00a3
+ * Stuktogram: https://progalap.elte.hu/stuki/v1/?uuid=07431a52-e4b2-4e4d-adba-7f5e406436a3
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Bekérés
+        int n;
+        int.TryParse(Console.ReadLine(), out n);
+        
+        int[] fogasok =  new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            int.TryParse(Console.ReadLine(), out fogasok[j]);
+        }
+        
+        
+        // Feldolgozás
+        int i = 1;
+        int ind = -1;
+        while (i < n && fogasok[i-1] != 0)
+        {
+            i++;
+        }
+
+        bool van = i < n;
+        if (van)
+        {
+            ind = i;
+        }
+
+
+        // Kiírás
+        Console.WriteLine($"{ind}");
+    }
+}
---
+++ ProgAlap/Gyak/KevesKonyv/KevesKonyv/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace KevesKonyv
+{
+/*
+ * Specifikáció: https://progalap.elte.hu/specifikacio/v1/?uuid=e66ec342-bf86-42e7-b132-af9f3674fed0
+ * Stuktogram: https://progalap.elte.hu/stuki/v1/?uuid=d662449a-e33d-46de-8900-f56849d8b112
+ */
+    class Program
+    {
+        struct Konyvtar
+        {
+            public string szerzo;
+            public string cim;
+            public int peldany;
+            public int kolcsonzott;
+        }
+        
+        static void Main(string[] args)
+        {
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+            Konyvtar[] k =  new Konyvtar[n];
+
+            for (int i = 0; i < n; i++) 
+            {
+                string[] sor = Console.ReadLine().Split(';');
+                
+                k[i].szerzo = sor[0];
+                k[i].cim = sor[1];
+                k[i].peldany = int.Parse(sor[2]);
+                k[i].kolcsonzott = int.Parse(sor[3]);
+            }
+
+            int db = 0;
+            int[] y = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (k[i].peldany - k[i].kolcsonzott <= 2)
+                {
+                    y[db] = i+1;
+                    db++;
+                }
+            }
+            
+            Console.Write(db);
+            for (int i = 0; i < db; i++)
+            {
+                Console.Write(" " + y[i]);
+            }
+        }
+    }
+}
